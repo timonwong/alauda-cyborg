62c088b1ddb10c3b04da538cab2ea653fa2650b2
Hang Yan <hangyan@alauda.io>
2018-12-05 16:10:13 +0800

    Update vendor

--- vendor/k8s.io/api/apps/v1/register.go
+++ vendor/k8s.io/api/apps/v1/register.go
@@ -41,19 +41,11 @@ var (
 	AddToScheme        = localSchemeBuilder.AddToScheme
 )
 
-// Adds the list of known types to the given scheme.
+// Adds the list of known types to api.Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
-		&Deployment{},
-		&DeploymentList{},
-		&StatefulSet{},
-		&StatefulSetList{},
 		&DaemonSet{},
 		&DaemonSetList{},
-		&ReplicaSet{},
-		&ReplicaSetList{},
-		&ControllerRevision{},
-		&ControllerRevisionList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
